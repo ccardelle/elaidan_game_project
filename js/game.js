58769f41e3223e604cfe189f790ac0c25dfc04b3
Chris Cardelle <chris.cardelle@gmail.com>
2019-06-03 12:30:25 -0400

    Added some styling

--- js/game.js
+++ js/game.js
@@ -1,23 +1,20 @@
- var config = { 
-     type: Phaser.AUTO,
-     width:800,
-     height:600,
-     physics: {
-         body: {
-             velocity : 0,
-         },
-        default: 'arcade',
-        arcade: {
-            gravtiy: {x : -1000, y: 0},
-            drawDebug : true,
-            debug: false,
-        }
-     },
-     scene: [Menu, Stage1, GameOver]
-    };
+var config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  parent: "canvas-window",
+  physics: {
+    body: {
+      velocity: 0
+    },
+    default: "arcade",
+    arcade: {
+      gravtiy: { x: -1000, y: 0 },
+      drawDebug: true,
+      debug: false
+    }
+  },
+  scene: [Menu, Stage1, GameOver]
+};
 
 var game = new Phaser.Game(config);
-
-
-
-
